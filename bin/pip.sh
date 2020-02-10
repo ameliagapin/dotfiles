507751f6f006fd891fb8e79109dea9b67b681c6f
amelia <amelia@entirelyamelia.com>
2020-02-10 20:12:08 +0000

    Some updates for bitly vm

--- bin/pip.sh
+++ bin/pip.sh
@@ -7,13 +7,13 @@ source bin/utils.sh || exit 1
 CONFIG=$1
 
 pecho "Updating pip...\n"
-sudo -H pip3 install --upgrade pip
-sudo -H pip2 install --upgrade pip
+pip3 install --upgrade pip
+pip2 install --upgrade pip
 
 pecho "Installing packages under Python3...\n"
-sudo -H pip3 install -v --no-cache-dir -r $CONFIG > /dev/null || exit 1
+pip3 install -v --no-cache-dir -r $CONFIG > /dev/null || exit 1
 
 pecho "Installing packages under Python2...\n"
-sudo -H pip install -v --no-cache-dir -r $CONFIG > /dev/null || exit 1
+pip install -v --no-cache-dir -r $CONFIG > /dev/null || exit 1
 
 exit 0
