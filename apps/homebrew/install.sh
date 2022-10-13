95823833bf70860d5f4bcba6d2fc4ee59d45d5aa
amelia gapin <amelia@entirelyamelia.com>
2022-10-13 13:15:52 -0400

    savepoint

--- apps/homebrew/install.sh
+++ apps/homebrew/install.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 set -e
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+which -s brew
+if [[ $? != 0 ]]; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+	eval "$(/opt/homebrew/bin/brew shellenv)"
 
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-echo "Please restart your terminal and continue with the install script"
+	echo "Please restart your terminal and continue with the install script"
+else
+	echo "Homebrew already installed, skipping"
+fi
 
 exit 0
