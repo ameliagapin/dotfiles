fef50ac1a740d7d8b97e3f623799443fb4699199
Amelia Gapin <amelia.gapin@agilebits.com>
2024-01-26 11:58:00 -0500

    make neo-tree slightly more nerdtree like

--- apps/nvim/nvim/lua/config/style.lua
+++ apps/nvim/nvim/lua/config/style.lua
@@ -102,8 +102,6 @@ require("catppuccin").setup({
             base = "#1a1a1a",
             mantle = "#181A1F",
             crust = "#2C323C",
-            -- mantle = "#1a1a1a",
-            -- crust = "#1a1a1a",
             green = "#C3E88D",
             red = "#ff5370",
             pink = "#ff869a",
--- apps/nvim/nvim/lua/plugins/telescope.lua
+++ apps/nvim/nvim/lua/plugins/telescope.lua
@@ -19,6 +19,8 @@ return {
         },
         keys= {
             { "<C-b>", "<cmd>Telescope buffers<cr>",    desc = "Show buffers in telescope" },
+            { "<C-p>", "<cmd>Telescope find_files<cr>",    desc = "Find files in telescope" },
+            { "<leader>/", "<cmd>Telescope live_grep<cr>",    desc = "Find files in telescope" },
         },
         config = function()
             local actions = require('telescope.actions')
@@ -31,7 +33,6 @@ return {
                             ['<C-j>'] = actions.move_selection_next,
                             ['<C-k>'] = actions.move_selection_previous,
                             ['<esc><esc>'] = actions.close,
-
                         },
                     },
                     file_ignore_patterns = { "/vendor/", "vendor/", "^vendor/" },
@@ -40,8 +41,8 @@ return {
             -- Enable telescope fzf native, if installed
             pcall(require('telescope').load_extension, 'fzf')
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
+            -- vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+            -- vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
         end
     },
 }
